abe6dbbf65d2bba9dec37a303aa48f205fc0daa2
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-27 12:30:41 +0530

    Create AddressBook Table

---
+++ UC2CreateAddressBookTable.sql
@@ -0,0 +1,3 @@
+use AddressBookService;
+create table AddressBook (firstName varchar(50), lastName varchar(50),Address varchar(100),City varchar(50),State varchar(50), Zip int, PhoneNo int, email varchar(50));
+select * from AddressBook;
