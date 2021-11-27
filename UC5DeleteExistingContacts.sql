56f398f7cd4869e77a508110cd13fa4dc3b64241
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-27 12:59:37 +0530

    Delete a person using person's name

---
+++ UC5DeleteExistingContacts.sql
@@ -0,0 +1,3 @@
+select * from AddressBook;
+insert into AddressBook (firstName,lastName,Address,City,State,Zip,PhoneNo,email) values ('Ram','Kumar','Kumarapalayam','Rasipuram','TamilNadu',636345,9555863377,'ram@gmail.com');
+delete from AddressBook where firstName = 'Ram';
